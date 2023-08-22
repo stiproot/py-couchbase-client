3966fdbf287e4d53353ff94e7bdb64f2202e859f
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-08-22 21:47:59 +0200

    fix:setup.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=install_requires,
     classifiers=[
         "Intended Audience :: Developers",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.11",
     ],
 )
